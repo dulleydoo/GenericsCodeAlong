7a3e80ff9c88d66e0f4bcb48e4adca6d20db49be
dulleydoo <karem.jabar2015@gmail.com>
2022-10-23 17:30:25 -0400

    Halfway

---
+++ src/main/java/genericclassesandinterfaces/before/AgeComparator.java
@@ -0,0 +1,11 @@
+package genericclassesandinterfaces.before;
+
+import java.util.Comparator;
+
+public class AgeComparator implements Comparator<Person>
+{
+    public int compare(final Person left, final Person right)
+    {
+        return Integer.compare(left.getAge(), right.getAge());
+    }
+}
---
+++ src/main/java/genericclassesandinterfaces/before/ReverseComparator.java
@@ -0,0 +1,19 @@
+package genericclassesandinterfaces.before;
+
+import java.util.Comparator;
+
+public class ReverseComparator<T> implements Comparator<T>
+{
+    private final Comparator<T> delegateComparator;
+
+    public ReverseComparator(final Comparator<T> delegateComparator)
+    {
+        this.delegateComparator = delegateComparator;
+    }
+
+    public int compare(final T left, final T right)
+    {
+        return -1 * delegateComparator.compare(left, right);
+    }
+}
+
